2acc142c3988d971b9e72aa3e0176c962211f440
TafsirAlNafin138 <2305138@ugrad.cse.buet.ac.bd>
2026-01-08 13:44:45 +0600

    previous modification was not added

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,20 @@
 import express from 'express';
 import 'dotenv/config'; 
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 console.log("This is my Kingdom");
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan());
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
